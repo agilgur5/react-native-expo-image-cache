b0b02243ee1d83483dac01e6c5e4be2c5e5dd21c
Anton Gilgur <agilgur5@gmail.com>
2019-02-25 02:48:27 -0500

    (pub): add dist files
    - will be using via GitHub installation for now since the og repo has
      not fixed these breaking issues in months (seems unmaintained)

---
+++ dist/src/index.js
@@ -0,0 +1,2 @@
+var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:true});Object.defineProperty(exports,"Image",{enumerable:true,get:function get(){return _Image.default;}});Object.defineProperty(exports,"CacheManager",{enumerable:true,get:function get(){return _CacheManager.default;}});var _Image=_interopRequireDefault(require("./Image"));var _CacheManager=_interopRequireDefault(require("./CacheManager"));
+//# sourceMappingURL=index.js.map
